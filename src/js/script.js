771ac3676f34decde3a06f7e75f0967b2ff4dc82
adiazrom <adiazrom26@gmail.com>
2025-03-31 20:37:03 +0200

    Fixed some accessibility issues

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,52 @@ console.log("Hello from script.js");
 
 let slideIndex = 1;
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function navigateSlides(event, direction) {
+  if (event.key === "ArrowLeft" && direction === -1) {
+      plusSlides(-1);
+  } else if (event.key === "ArrowRight" && direction === 1) {
+      plusSlides(1);
+  } else if (event.key === "Enter" || event.key === " ") {
+      plusSlides(direction);
+  }
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("featured-preview");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {
+    slideIndex = 1
+}
+  
+  if (n < 1) {
+    slideIndex = slides.length
+}
+
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM loaded");
 
@@ -41,38 +87,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("featured-preview");
-  let dots = document.getElementsByClassName("dot");
-  
-  if (n > slides.length) {
-    slideIndex = 1
-}
-  
-  if (n < 1) {
-    slideIndex = slides.length
-}
-
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
